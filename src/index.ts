0875a7708c79135c6dfd7a03edfcba0449a94128
Stefano Peeters <stefano.peeters@wolterskluwer.com>
2021-11-26 22:06:14 +0100

    wip: adding deploy to vercel functionality

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,12 @@ interface IOption {
  */
 class PelckmansCli extends Command {
   static args = [{name: 'name'}];
-  static description = 'Generate a static site and deploy it to Netlify ';
+  static description = 'Generate a static site (and deploy it to Netlify)';
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   };
-
   static strict = false;
-  // TODO: read from json file
   protected boilerplates: IOption = {
     nextjs: 'https://github.com/PsySolix/next-boilerplate',
     memoria: 'https://github.com/PsySolix/memoria-boilerplate',
@@ -39,45 +32,42 @@ class PelckmansCli extends Command {
    */
   async run() {
     const {args} = this.parse(PelckmansCli);
-    this.log(JSON.stringify(args));
-    // Get projectName
-    // let name = await cli.prompt("What is the project name?");
     const name = args.name === '' ? 'unknown-project' : args.name;
 
     // Get projectType
     const {type}: { type: string } = await inquiry(
-        'type',
-        'Select a project type',
-        Object.keys(this.boilerplates).map((key) => ({
-          name: key,
-        })),
+      'type',
+      'Select a project type',
+      Object.keys(this.boilerplates).map((key) => ({
+        name: key,
+      })),
     );
 
     // Get theme
     const {theme}: { theme: string } = await inquiry(
-        'theme',
-        'Select a theme',
-        Object.keys(this.themes).map((key) => ({
-          name: key,
-        })),
+      'theme',
+      'Select a theme',
+      Object.keys(this.themes).map((key) => ({
+        name: key,
+      })),
     );
 
     // Deployment;
     const {deploy}: { deploy: string } = await inquiry(
-        'deploy',
-        'Select a deployment target:',
-        ['vercel', 'none'],
+      'deploy',
+      'Select a deployment target:',
+      ['vercel', 'none'],
     );
-    this.log(deploy);
 
     // Generate project
     const project = new Project(
-        name.toLowerCase(),
-        type.toLowerCase(),
-        this.themes[theme],
-        theme,
-        this.boilerplates[type],
-        this.log,
+      name.toLowerCase(),
+      type.toLowerCase(),
+      this.themes[theme],
+      theme,
+      this.boilerplates[type],
+      deploy,
+      this.log,
     );
 
     await project.init();
--- src/project.ts
+++ src/project.ts
@@ -1,4 +1,6 @@
 // has no default export
+import {exec} from 'child_process';
+
 const git = require('nodegit');
 import * as fs from 'fs';
 import * as path from 'path';
@@ -11,8 +13,9 @@ const {spawn} = require('child_process');
  * Project class
  */
 export class Project {
-  logger: Function;
+  log: Function;
   name: string;
+  deploy: string;
   themeName: string;
   themePath: string;
   type: string;
@@ -26,23 +29,26 @@ export class Project {
    * @param {string} themePath
    * @param {string} themeName
    * @param {string} repo
+   * @param {string} deploy
    * @param {function} logFn
    */
   constructor(
-      name: string,
-      type: string,
-      themePath: string,
-      themeName: string,
-      repo: string,
-      logFn: Function,
+    name: string,
+    type: string,
+    themePath: string,
+    themeName: string,
+    repo: string,
+    deploy: string,
+    logFn: Function,
   ) {
     this.name = name;
     this.type = type;
     this.themePath = themePath;
     this.themeName = themeName;
     this.repo = repo;
-    this.dir = `./__PROJECTS__/${name}`;
-    this.logger = logFn;
+    this.deploy = deploy;
+    this.dir = `${process.cwd()}/${name}`;
+    this.log = logFn;
   }
 
   /**
@@ -59,7 +65,7 @@ export class Project {
       spinner('stop', `DONE`);
 
       spinner('start', `# Installing dependencies for ${this.name}`);
-      await this.installDependancies();
+      await this.installDependencies();
       spinner('stop', `DONE`);
 
       spinner('start', `# Applying theme: ${this.themePath}`);
@@ -69,11 +75,45 @@ export class Project {
       spinner('start', `# Applying Environment variables: ${this.themePath}`);
       this.applyEnvVars();
       spinner('stop', `DONE`);
+
+      spinner('start', `# Deploying with ${this.deploy}`);
+      await this.deployProject();
+      spinner('stop', 'DONE');
     } catch (error) {
       this.triggerCliError((error as Error).message);
     }
   }
 
+  /**
+   * Deploy project with selected deployment
+   * @protected
+   */
+  protected async deployProject(): Promise<void> {
+    if (this.deploy === 'none') {
+      return;
+    }
+    if (this.deploy === 'vercel') {
+      const cliDir = process.cwd();
+      process.chdir(this.dir);
+      return new Promise((resolve, reject)=> {
+        let error = '';
+        const vercelCmds = exec('npm i vercel && vercel');
+        vercelCmds.stdout.on('data', (data) => {
+          this.log(data.toString());
+        });
+        vercelCmds.stderr.on('data', (data) => {
+          error = data.toString();
+        });
+        vercelCmds.on('close', (code) => {
+          if (code === 0) {
+            resolve();
+          }
+          reject(new Error(error));
+        });
+        process.chdir(cliDir);
+      });
+    }
+  }
   /**
    * Adds a .env file (that the boilerplates use) which contains the following:
    * PROJECT_TYPE
@@ -94,13 +134,13 @@ export class Project {
   protected async applyTheme(): Promise<void> {
     fs.copyFileSync(this.themePath, `${this.themePath}-copy`);
     return fs.rename(
-        `${this.themePath}-copy`,
-        path.resolve(this.dir, 'style.css'),
-        (err) => {
-          if (err) {
-            throw err;
-          }
-        },
+      `${this.themePath}-copy`,
+      path.resolve(this.dir, 'style.css'),
+      (err) => {
+        if (err) {
+          throw err;
+        }
+      },
     );
   }
 
@@ -116,7 +156,7 @@ export class Project {
    * Installs the dependencies in the project dir
    * (only if there is package.json present)
    */
-  protected async installDependancies(): Promise<void> {
+  protected async installDependencies(): Promise<void> {
     const cliDir = process.cwd();
     const packageJsonFile = path.resolve(this.dir, 'package.json');
 
@@ -157,7 +197,7 @@ export class Project {
       } while (suffix <= 10 && fs.existsSync(memDir));
       if (fs.existsSync(memDir)) {
         throw new Error(
-            `No suitable directory has been found for ${this.name}`,
+          `No suitable directory has been found for ${this.name}`,
         );
       }
       this.dir = memDir;
@@ -171,7 +211,7 @@ export class Project {
    * @protected
    */
   protected triggerCliError(msg: string, code?: number): void {
-    this.logger(msg);
+    this.log(msg);
     exit(code ? code : 1);
   }
 }
